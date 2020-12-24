0c822d985f332656e403e747eec03ad2762a3d42
Vladimir Makhmutov <makhmutov.va@mail.ru>
2020-12-24 15:57:35 +0700

    Чистый проект с настроенной авторизацией и базой

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyBpOsum82lPm6H3jLdcTTsrYOEJT53jBP8',
+  fbDbUrl: 'https://angular-blog-acf7b-default-rtdb.europe-west1.firebasedatabase.app'
+};
